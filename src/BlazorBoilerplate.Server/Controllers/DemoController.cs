497063032f2adf1f78af4951bc502b5558bd6835
Nicolas Stohler <nicolas.stohler@ensi.ch>
2019-06-24 14:04:52 +0200

    add settings.Production.json support, iis deploy works

---
+++ src/BlazorBoilerplate.Server/Controllers/DemoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorBoilerplate.Server.Controllers
+{
+    [Route("api/demo")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Message = "wusel"
+            });
+        }
+
+        [HttpPost("dusel")]
+        public IActionResult Dusel()
+        {
+            return Ok(new
+            {
+                Message = "dusel"
+            });
+        }
+
+        [HttpPost("hallo")]
+        public IActionResult Hallo()
+        {
+            return Ok(new
+            {
+                Message = "dusel"
+            });
+        }
+    }
+}
